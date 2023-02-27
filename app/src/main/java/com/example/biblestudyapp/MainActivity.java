cf916790c95ebf6adfa46016beb289ada513a4b4
MMark101 <mark.a.mclaughlin@my.wheaton.edu>
2023-02-27 14:44:42 -0600

    Added FireBase

--- app/src/main/java/com/example/biblestudyapp/MainActivity.java
+++ app/src/main/java/com/example/biblestudyapp/MainActivity.java
@@ -3,12 +3,34 @@ package com.example.biblestudyapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
+    FirebaseFirestore firestore;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        firestore = FirebaseFirestore.getInstance();
+
+        Map<String,Object> users = new HashMap<>();
+        users.put("Mark","Mc");
+        users.put("Josh", "H");
+
+        firestore.collection("users").add(users).addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+            @Override
+            public void onSuccess(DocumentReference documentReference) {
+                Toast.makeText(getApplicationContext(),"Success", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
