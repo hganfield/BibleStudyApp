7c694b4e6ca25423045a888850c60e77224a5957
hganfield <heather.ganfield@my.wheaton.edu>
2023-02-28 18:22:23 -0600

    Bible html webview linked--needs to be replaced with API

--- app/src/main/java/com/example/biblestudyapp/BiblePage.java
+++ app/src/main/java/com/example/biblestudyapp/BiblePage.java
@@ -3,6 +3,8 @@ package com.example.biblestudyapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.webkit.WebView;
+
 
 public class BiblePage extends AppCompatActivity {
 
@@ -10,5 +12,8 @@ public class BiblePage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bible_page);
+
+        WebView bible = (WebView) findViewById(R.id.Bible);
+        bible.loadUrl("file:///android_asset/bible.html");
     }
 }
