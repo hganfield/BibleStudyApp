67a11c6a4199d8003b74264c70d3264aa14d4fc1
MMark101 <mark.a.mclaughlin@my.wheaton.edu>
2023-03-20 15:17:51 -0500

    Progress on creating a search function

--- app/src/main/java/com/example/biblestudyapp/InviteUsersFragment.java
+++ app/src/main/java/com/example/biblestudyapp/InviteUsersFragment.java
@@ -2,11 +2,25 @@ package com.example.biblestudyapp;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -55,10 +69,47 @@ public class InviteUsersFragment extends Fragment {
         }
     }
 
+
+    RecyclerView recyclerView;
+    List<User> usersList;
+    FirebaseAuth firebaseAuth;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_invite_users, container, false);
+        View view = inflater.inflate(R.layout.fragment_invite_users, container, false);
+        recyclerView = view.findViewById(R.id.recycleView);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        usersList = new ArrayList<>();
+        firebaseAuth = FirebaseAuth.getInstance();
+
+
+        return view;
+    }
+    private void getAllUsers() {
+        final FirebaseUser firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("Users");
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                usersList.clear();
+                for (DataSnapshot dataSnapshot1 : dataSnapshot.getChildren()) {
+                    User user = dataSnapshot1.getValue(User.class);
+                    if (user.getUid() != null && !user.getUid().equals(firebaseUser.getUid())) {
+                        usersList.add(user);
+                    }
+                    //adapterUsers = new AdapterUsers(getActivity(), usersList);
+                    //recyclerView.setAdapter(adapterUsers);
+                }
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 }
